480b3be055ac6df05c4c217cc7e96a5dc47ccabe
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-22 00:45:04 -0400

    added styling to left side of login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import '../scss/styles.scss';
 
 function Login() {
-  return <div className='login'>Login</div>;
+  return (
+    <div className='login'>
+      <div className='card'>
+        <div className='left'>
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa,
+            soluta maiores blanditiis fugiat sint harum quisquam at debitis
+            fugit reprehenderit libero!
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className='right'>
+          <h1>Login</h1>
+          <form>
+            <input type='text' placeholder='Username' />
+            <input type='password' placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
